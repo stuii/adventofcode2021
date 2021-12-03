65c4e582f68356a3d831e7d8f5804fa1343ffc6b
stui <me@stui.ch>
2021-12-03 14:45:48 +0100

    tiny bit linting

--- day3/part2.php
+++ day3/part2.php
@@ -1,6 +1,6 @@
 <?php
 
-    $input = explode("\r\n",trim(file_get_contents('./input.txt')));
+    $input = explode("\r\n", trim(file_get_contents('./input.txt')));
     $oxygen = $input;
     $co2 = $input;
 
@@ -8,11 +8,11 @@
     function getCommon(array $array, int $offset, bool $most = true)
     {
         $common = [0 => 0, 1 => 0];
-        foreach($array as $entry){
-            $common[substr($entry,$offset,1)]++;
+        foreach ($array as $entry) {
+            $common[substr($entry, $offset, 1)]++;
         }
 
-        if($most) {
+        if ($most) {
             return $common[0] > $common[1] ? 0 : 1;
         } else {
             return $common[0] <= $common[1] ? 0 : 1;
@@ -20,24 +20,24 @@
     }
 
 
-    for($offset = 0; $offset < strlen($input[0]); $offset++) {
-            if (count($oxygen) > 1) {
-                $mostCommon = getCommon($oxygen, $offset, true);
-                foreach ($oxygen as $key => $rate) {
-                    if (substr($rate, $offset, 1) != $mostCommon) {
-                        unset($oxygen[$key]);
-                    }
+    for ($offset = 0; $offset < strlen($input[0]); $offset++) {
+        if (count($oxygen) > 1) {
+            $mostCommon = getCommon($oxygen, $offset, true);
+            foreach ($oxygen as $key => $rate) {
+                if (substr($rate, $offset, 1) != $mostCommon) {
+                    unset($oxygen[$key]);
                 }
             }
+        }
 
-            if (count($co2) > 1) {
-                $leastCommon = getCommon($co2, $offset, false);
-                foreach ($co2 as $key => $rate) {
-                    if (substr($rate, $offset, 1) != $leastCommon) {
-                        unset($co2[$key]);
-                    }
+        if (count($co2) > 1) {
+            $leastCommon = getCommon($co2, $offset, false);
+            foreach ($co2 as $key => $rate) {
+                if (substr($rate, $offset, 1) != $leastCommon) {
+                    unset($co2[$key]);
                 }
             }
+        }
     }
 
     $oxygen = array_shift($oxygen);
